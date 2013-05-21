ad7169cd9b00d93a79010c549f9c3d61982eeaf5
Jiahao Liu <jiahaoliuliu@gmail.com>
2013-05-21 11:11:38 +0200

    The first version of the application

--- src/es/altran/android/activitylifecycle/MainActivity.java
+++ src/es/altran/android/activitylifecycle/MainActivity.java
@@ -2,21 +2,54 @@ package es.altran.android.activitylifecycle;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		context = this;
 		setContentView(R.layout.activity_main);
+		Toast.makeText(context, "onCreate", Toast.LENGTH_LONG).show();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    protected void onStart() {
+    	super.onStart();
+        Toast.makeText(context, "onStart", Toast.LENGTH_LONG).show();
+    }
 
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	Toast.makeText(context, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	Toast.makeText(context, "onPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	Toast.makeText(context, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+    	Toast.makeText(context, "onDestroy", Toast.LENGTH_LONG).show();
+    	super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+    	Toast.makeText(context, "onRestart", Toast.LENGTH_LONG).show();
+    	super.onRestart();
+    }
 }
